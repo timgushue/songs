1bf2ccd8fb44b7588f08bbf6cc5e48a6a26ec708
timgushue <gushue.tim@gmail.com>
2013-08-23 12:39:46 -0700

    added database configuration

--- main.rb
+++ main.rb
@@ -2,6 +2,14 @@ require 'sinatra'
 require 'slim'
 require './song'
 
+configure :development do
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 configure do
 	enable :sessions
 	set :username, 'frank'
